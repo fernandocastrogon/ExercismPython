44b7e58eda30b09a09452a08b61c5932e2b413f2
Fernando Castro <fernandocastrogon@gmail.com>
2024-08-19 23:37:11 -0300

    improved value_of_card function

--- BlackJack.py
+++ BlackJack.py
@@ -16,35 +16,9 @@ def value_of_card(card):
     3.  '2' - '10' = numerical value.
     """
 
-    match card:
-        case '2':
-            return 2
-        case '3':
-            return 3
-        case '4':
-            return 4
-        case '5':
-            return 5
-        case '6':
-            return 6
-        case '7':
-            return 7
-        case '8':
-            return 8
-        case '9':
-            return 9
-        case '10':
-            return 10
-        case 'A':
-            return 1
-        case 'J':
-            return 10
-        case 'Q':
-            return 10
-        case 'K':
-            return 10
-        case _:
-            print("Invalid Card")
+    if (card in 'JQK'): return 10
+    if (card == 'A'): return 1
+    return int(card)
 
 def higher_card(card_one, card_two):
     """Determine which card has a higher value in the hand.
